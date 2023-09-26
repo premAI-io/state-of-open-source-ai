1aa15d6524ef8e7140eab88d0789bae20adaa749
Casper da Costa-Luis <casper.dcl@physics.org>
2023-09-26 11:14:48 +0100

    remove external markers on source repo links

--- _static/external_target.js
+++ _static/external_target.js
@@ -1,4 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
   /// open external links in new tabs
-  document.querySelectorAll('a.reference.external').forEach(a => { a.target = '_blank'; });
+  document.querySelectorAll('a.reference.external').forEach(a => {
+    a.target = '_blank';
+    if (a.href.startsWith("https://github.com/premAI-io/state-of-open-source-ai")){
+      a.classList.replace('external', 'internal');
+    }
+  });
 });

cbce2f000b56028c02673992b177c8dc1b0f82e2
nsosio <sosio.nicola94@tiscali.it>
2023-09-30 15:51:14 +0200

    created plugin extending UnsrtStyle in order to limit the number of maximum authors shown in the bibliography

---
+++ bibliography.py
@@ -0,0 +1,203 @@
+from pybtex.plugin import register_plugin
+from sphinx.application import Sphinx
+from pybtex.style.formatting import toplevel
+from pybtex.style.formatting.unsrt import Style as UnsrtStyle
+from pybtex.style.formatting.unsrt import date, pages
+from pybtex.style.template import (
+    field, first_of, join, optional, optional_field, sentence, tag, words
+)
+
+
+class Style(UnsrtStyle):
+
+    def __init__(self, max_authors=5, label_style=None, name_style=None, sorting_style=None, abbreviate_names=False, min_crossrefs=2, **kwargs):
+        self.max_authors = max_authors
+        super().__init__(label_style=label_style, name_style=name_style, sorting_style=sorting_style, abbreviate_names=abbreviate_names, min_crossrefs=min_crossrefs, **kwargs)
+
+    def get_article_template(self, e):
+        volume_and_pages = first_of [
+            # volume and pages, with optional issue number
+            optional [
+                join [
+                    field('volume'),
+                    optional['(', field('number'),')'],
+                    ':', pages
+                ],
+            ],
+            # pages only
+            words ['pages', pages],
+        ]
+        template = toplevel [
+            self.format_names('author'),
+            self.format_title(e, 'title'),
+            sentence [
+                tag('em') [field('journal')],
+                optional[ volume_and_pages ],
+                date],
+            sentence [ optional_field('note') ],
+            self.format_web_refs(e),
+        ]
+        return template
+
+    def format_author(self, e, as_sentence = True):
+        authors = self.format_names('author', as_sentence=False)
+        if 'author' not in e.persons:
+            return authors
+        if len(e.persons['author']) > self.max_authors:
+            word = 'et al'
+            e.persons['author'] = e.persons['author'][:self.max_authors]
+            result = join(sep=', ')[authors, word]
+        else:
+            result = authors
+        if as_sentence:
+            return sentence[result]
+        else:
+            return result
+
+    def format_author_or_editor(self, e):
+        return first_of [
+            optional[ self.format_author(e) ],
+            self.format_editor(e),
+        ]
+
+    def get_booklet_template(self, e):
+        template = toplevel [
+            self.format_author(e),
+            self.format_title(e, 'title'),
+            sentence [
+                optional_field('howpublished'),
+                optional_field('address'),
+                date,
+                optional_field('note'),
+            ],
+            self.format_web_refs(e),
+        ]
+        return template
+
+    def get_incollection_template(self, e):
+        template = toplevel [
+            sentence [self.format_author(e)],
+            self.format_title(e, 'title'),
+            words [
+                'In',
+                sentence [
+                    optional[ self.format_editor(e, as_sentence=False) ],
+                    self.format_btitle(e, 'booktitle', as_sentence=False),
+                    self.format_volume_and_series(e, as_sentence=False),
+                    self.format_chapter_and_pages(e),
+                ],
+            ],
+            sentence [
+                optional_field('publisher'),
+                optional_field('address'),
+                self.format_edition(e),
+                date,
+            ],
+            self.format_web_refs(e),
+        ]
+        return template
+
+    def get_inproceedings_template(self, e):
+        template = toplevel [
+            sentence [self.format_author(e)],
+            self.format_title(e, 'title'),
+            words [
+                'In',
+                sentence [
+                    optional[ self.format_editor(e, as_sentence=False) ],
+                    self.format_btitle(e, 'booktitle', as_sentence=False),
+                    self.format_volume_and_series(e, as_sentence=False),
+                    optional[ pages ],
+                ],
+                self.format_address_organization_publisher_date(e),
+            ],
+            sentence [ optional_field('note') ],
+            self.format_web_refs(e),
+        ]
+        return template
+
+    def get_mastersthesis_template(self, e):
+        template = toplevel [
+            sentence [self.format_author(e)],
+            self.format_title(e, 'title'),
+            sentence[
+                "Master's thesis",
+                field('school'),
+                optional_field('address'),
+                date,
+            ],
+            sentence [ optional_field('note') ],
+            self.format_web_refs(e),
+        ]
+        return template
+
+    def get_misc_template(self, e):
+        template = toplevel [
+            optional[ sentence [self.format_author(e)] ],
+            optional[ self.format_title(e, 'title') ],
+            sentence[
+                optional[ field('howpublished') ],
+                optional[ date ],
+            ],
+            sentence [ optional_field('note') ],
+            self.format_web_refs(e),
+        ]
+        return template
+
+    def get_online_template(self, e):
+        return self.get_misc_template(e)
+
+    def get_phdthesis_template(self, e):
+        template = toplevel [
+            sentence [self.format_author(e)],
+            self.format_btitle(e, 'title'),
+            sentence[
+                first_of [
+                    optional_field('type'),
+                    'PhD thesis',
+                ],
+                field('school'),
+                optional_field('address'),
+                date,
+            ],
+            sentence [ optional_field('note') ],
+            self.format_web_refs(e),
+        ]
+        return template
+
+    def get_techreport_template(self, e):
+        template = toplevel [
+            sentence [self.format_author(e)],
+            self.format_title(e, 'title'),
+            sentence [
+                words[
+                    first_of [
+                        optional_field('type'),
+                        'Technical Report',
+                    ],
+                    optional_field('number'),
+                ],
+                field('institution'),
+                optional_field('address'),
+                date,
+            ],
+            sentence [ optional_field('note') ],
+            self.format_web_refs(e),
+        ]
+        return template
+
+    def get_unpublished_template(self, e):
+        template = toplevel [
+            sentence [self.format_author(e)],
+            self.format_title(e, 'title'),
+            sentence [
+                field('note'),
+                optional[ date ]
+            ],
+            self.format_web_refs(e),
+        ]
+        return template
+
+
+def setup(app: Sphinx):
+    register_plugin('pybtex.style.formatting', 'unsrt_max_authors', Style)

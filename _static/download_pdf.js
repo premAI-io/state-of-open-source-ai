adc4b068d2fa667d966264ffe52ca02bf8ad98af
Casper da Costa-Luis <casper.dcl@physics.org>
2023-10-13 22:53:55 +0100

    format: drop .md download button

--- _static/download_pdf.js
+++ _static/download_pdf.js
@@ -3,4 +3,8 @@ document.addEventListener('DOMContentLoaded', function(){
   document.querySelectorAll('.btn-download-pdf-button').forEach(a => {
       a.onclick = () => window.open("/state-of-open-source-ai.pdf");
   });
+  /// hide Markdown button
+  document.querySelectorAll('.btn-download-source-button').forEach(a => {
+    a.parentElement.style.display = 'none';
+  });
 });
